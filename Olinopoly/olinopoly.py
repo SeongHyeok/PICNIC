a68e8bef42ecae4f662ed84477d50888e148238b
SeongHyeok Im <ishez9210@gmail.com>
2014-04-06 21:41:39 -0400

    Add initial code.

---
+++ Olinopoly/olinopoly.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Olinopoly: A board game for Olin!
+SoftwareDesign Project 2014 Spring
+
+Created on Sun Apr  6 21:34:32 2014
+
+@author: SeongHyeok Im, Inseong Joe and Dongyoung Kang
+
+@note:
+    Code-style:
+        - Camelcase for function and class.
+        - Underscore for variable.
+        - Prefix 'g_' for global viarlable.
+"""
+
+############################################################################
+# Imports
+############################################################################
+
+import pygame
+from pygame.locals import *
+import random
+import math
+import time
+import sys, traceback
+
+############################################################################
+# Global variabless
+############################################################################
+
+# Screen
+g_screen_width = 800
+g_screen_height = 800
+
+############################################################################
+# Main
+############################################################################
+
+if __name__ == "__main__":
+    pygame.init()
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+                break
+
+    pygame.quit()
